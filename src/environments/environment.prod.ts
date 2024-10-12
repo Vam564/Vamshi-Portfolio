1f5a4bd2dd2bfaddb886c6f0f802bf6c9b1b52c2
Vamshi Kumar <vamshikumar@Vamshis-MBP.lan>
2024-10-12 00:20:02 -0500

    resolving build errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseUrl : `${window.location.protocol}//${window.location.hostname}/portfolio/`,
+  crypto : 'export NODE_OPTIONS=--openssl-legacy-provider'
 };
